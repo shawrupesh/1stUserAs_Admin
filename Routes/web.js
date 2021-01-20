7b07c6b9d2c7d649456b6ddad0774ff920625275
Rupesh kumar shaw <shawrupesh020@gmail.com>
2021-01-21 01:39:49 +0530

    web

--- Routes/web.js
+++ Routes/web.js
@@ -1,9 +1,5 @@
 
 const newRegister=require('../app/http/controllers/newRegister')
-//const getBalance=require('../app/http/controllers/GetBalance')
-
-//const addFund = require('../app/http/controllers/addFund')
-//const spendFund = require('../app/http/controllers/spendFund')
 const getAllUser= require('../app/http/controllers/allUser')
 const getAllRoles= require('../app/http/controllers/UserRoles')
 const start= require('../app/http/controllers/start')
@@ -20,9 +16,7 @@ function initRoutes(app) {
 
 
     //Post request Routes
-   /// app.post('/addfund',addFund().postAddFund)
-   // app.post('/spendfund',spendFund().postSpend)
-   // app.post('/balance',getBalance().Getbalance)
+   
     app.post('/register',newRegister().postRegister)
 
 }
